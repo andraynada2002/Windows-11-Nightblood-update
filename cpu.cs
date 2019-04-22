ad2c530ca3cf9956be6c624ccfb8d817d8b0e524
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-23 09:36:52 +1000

    Add files via upload

---
+++ cpu.cs
@@ -0,0 +1,26 @@
+private class cpu;
+public class cpu;
+class cpu;
+
+//CPU optimized
+//CPU cores optimized
+//CPU load optimized
+//CPU performance optimized
+//CPU repaired performance
+namespace cpu;
+{
+	void cpu()
+	void cores()
+	void load()
+	void performance()
+	private void cpu()
+	public void cpu()
+	int 3;
+	private int 2;
+	public int 1;
+	protected cpu;
+	private protected cpu;
+	public protected cpu;
+}
+
+
