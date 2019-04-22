3be561455c3b74a92a20b0c0e33f3fa43a7b671c
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-23 09:32:32 +1000

    Add files via upload

---
+++ windowsxi.cs
@@ -0,0 +1,166 @@
+class windowsxi;
+public class windowsxi;
+private class windowsxi;
+
+//Windows 11 type os
+//Windows 11 type windows 10
+//Windows 11 type windows operating system
+//Windows 11 optimized
+//Windows 11 optimized core
+//Windows 11 optimized shell
+//Windows 11 optimized CPU cores
+//Windows 11 optimized GPU cores
+//Windows 11 optimized subsurface
+//Windows 11 optimized desktop
+//Windows 11 optimized applications
+//Windows 11 optimized standalone applications
+//Windows 11 optimized CPU
+//Windows 11 optimized GPU
+//Windows 11 optimized RAM
+//Windows 11 optimized Wi-Fi
+//Windows 11 optimized Ethernet
+//Windows 11 boosted
+//Windows 11 Wi-Fi speed traced
+//Windows 11 Wi-Fi ping traced
+//Windows 11 Wi-Fi connection traced
+//Windows 11 Wi-Fi signal traced
+//Windows 11 Ethernet speed traced
+//Windows 11 Ethernet ping traced
+//Windows 11 Ethernet connection traced
+//Windows 11 Wi-Fi signal hight
+//Windows 11 Wi-Fi signal ultra
+//Windows 11 performance optimized
+//Windows 11 services optimized
+//Windows 11 start menu registry optimized
+//Windows 11 Wi-Fi registry optimized
+//Windows 11 Ethernet registry optimized
+//Windows 11 core registry optimized
+//Windows 11 shell registry optimized
+//Windows 11 Google Chrome registry optimized
+//Windows 11 Google Chrome optimized
+//Windows 11 applications registry optimized
+//Windows 11 bios registry optimized
+//Windows 11 drivers registry optimized
+//Windows 11 display driver registry optimized
+//Windows 11 boot registry optimized
+//Windows 11 reboot registery optimized
+//Windows 11 Turn Off registery optimized
+namespace windowsxi;
+{
+	void windowsxi()
+	void optimization()
+	void windows11()
+	void core()
+	void shell()
+	public void windowsxi()
+	private void windowsxi()
+	int 1;
+	public int 2;
+	private int 3;
+	protected windowsxi()
+	public protected windowsxi()
+	private protected windowsxi()
+	object array 20;
+	public object array 21;
+	private object array 22;
+	float 1;
+	public float 2;
+	private float 3;
+	bool 0;
+	public bool 2;
+	private bool 3;
+	sbyte 59;
+	public sbyte 58;
+	private sbyte 53;
+	byte 2048;
+	public byte 4096;
+	private byte 8192;
+	object get 0x2;
+	object get 0x3;
+	object get 0x1;
+	public object get 0x4;
+	private object get 0x6
+	double 2;
+	public double 3;
+	private double 4;
+	params windowsxi;
+	public params windowsxi;
+	private params windowsxi;
+	value 2;
+	public value 1;
+	private value 0;
+	abstract windowsxi;
+	public abstract windowsxi;
+	private abstract windowsxi;
+	sealed windowsxi;
+	public sealed windowsxi;
+	private sealed windowsxi;
+	async windows11;
+	public async windows11;
+	private async windowsxi;
+	base windows11;
+	public base windows11;
+	private base windows11;
+	break windows11;
+	public break windows11;
+	private break windows11;
+	static windows11;
+	public static windows11;
+	private static windows11;
+	while windows11;
+	public while windows11;
+	private while windows11;
+	await windows11;
+	public await windows11;
+	private await windows11;
+	var windowsxi;
+	public var windowsxi;
+	private var windowsxi;
+	virtual windowsxi;
+	public virtual windowsxi;
+	private virtual windowsxi;
+	struct windows11;
+	public struct windows11;
+	private struct windows11;
+	operator windows11;
+	public operator windows11;
+	private operator windows11;
+	this windows11;
+	public this windows11;
+	private this windows11;
+	true windows11;
+	private true windows11;
+	public true windows11;
+	false windows11;
+	private false windows11;
+	public false windows11;
+	stackalloc windows11;
+	private stackalloc windows11;
+	public stackalloc windows11;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
