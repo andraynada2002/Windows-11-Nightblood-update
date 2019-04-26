3a1b0063f13ddb4754c0f76a1db5c15f9e2a73ae
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-26 23:08:50 +1000

    Add files via upload

---
+++ ram.cs
@@ -0,0 +1,39 @@
+class ram;
+public class ram;
+private class ram;
+
+//RAM optimized
+//RAM boosted
+//RAM type DDR3
+//RAM type DDR4
+//RAM optimized performance
+//RAM repaired performance
+//RAM hight performance
+//RAM optimized framerate
+//RAM optimized
+//RAM type physical memory
+namespace ram;
+{
+	void ram()
+	public void ram()
+	private void ram()
+	int 2;
+	private int 3;
+	public int 4;
+	protected ram()
+	public protected ram()
+	private protected ram()
+	public set ram;
+	public get ram;
+	public add ram;
+	set ram;
+	get ram;
+	add ram;
+	private set ram;
+	private add ram;
+	private get ram;
+}
+
+
+
+
