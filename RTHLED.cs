21200351bc79b8a6d4368ae9c09b9faa597a5c3a
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-26 21:48:30 +1000

    Add files via upload

---
+++ RTHLED.cs
@@ -0,0 +1,34 @@
+public class rthled;
+private class rthled;
+class rthled;
+
+//RTHLED type LED
+//RTHLED type Display
+//RTHLED HDR histogram alignment
+//RTHLED HDR histogram
+//RTHLED Ray Traced histogram
+//RTHLED Ray Traced histogram
+//RTHLED Ray Traced contrast
+//RTHLED Ray Traced bridthness
+namespace rthled;
+{
+	void rthled;
+	public void rthled;
+	private void rthled;
+	public int 2;
+	int 3;
+	private int 5;
+	public protected rthled;
+	private protected rthled;
+	protected rthled;
+	set rthled;
+	public set rthled;
+	private set rthled;
+	public get rthled;
+	get rthled;
+	private get rthled;
+	public add rthled;
+	private add rthled;
+	add rthled;
+}
+
