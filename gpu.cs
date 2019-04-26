e786300d50f0f0a3bf082eba5231fe72f32ec9fa
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-26 21:43:28 +1000

    Add files via upload

---
+++ gpu.cs
@@ -0,0 +1,35 @@
+public class gpu;
+private class gpu;
+class gpu;
+
+//GPU type processor
+//GPU type graphics processor
+//GPU optimized
+//GPU hight performance
+//GPU upgrading graphics
+//GPU upgrade monitor RGB
+//GPU HDR contrast monitor RGB
+//GPU optimized performance
+//GPU repaired performance
+//GPU fixed graphics
+//GPU fixed DirectX graphics
+//GPU fixed Vulkan graphics
+//GPU HDR bridthness monitor RGB
+//GPU HDR histogram alignment
+//GPU HDR histogram 
+namespace gpu;
+{
+	public int 2;
+	public void gpu()
+	public protected gpu()
+	private int 3;
+	private void gpu()
+	private protected gpu()
+	public set gpu;
+	public add gpu;
+	public get gpu;
+	private set gpu;
+	private get gpu;
+	private add gpu;
+}
+
