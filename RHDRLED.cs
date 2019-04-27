6e77cdd0af5f032ac5af607ebb139682cf3915b8
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-27 12:38:49 +1000

    Add files via upload

---
+++ RHDRLED.cs
@@ -0,0 +1,36 @@
+public class rhhdrled;
+private class rhdrled;
+class rhdrled;
+
+//rthled type LED
+//rthled type Display
+//rthled type Display Screen
+//rthled optimized performance
+//rthled optimized colors
+//rthled optimized rgb
+//rthled HDR rgb
+//rthled HDR contrast rgb
+//rthled HDR bridthness rgb 
+//rthled HDR gamma rgb
+//rthled ray traced rgb
+//rthled ray traced colors rgb 
+//rthled ray traced bridthness rgb
+//rthled ray traced contrast rgb 
+//rthled ray traced gamma rgb
+//rthled hdr colors rgb
+namespace rthled;
+{
+	public void rthled()
+	private void rthled()
+	public int 2;
+	private int 3;
+	public protected rthled;
+	private protected rthled;
+	public get rthled;
+	private get rthled;
+	public set rthled;
+	private set rthled;
+	public add rthled;
+	private add rthled;
+}
+
