b64011380a4fc94fbdb562bf2e25ef7feb70369a
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-30 09:26:42 +1000

    Add files via upload

---
+++ font.cs
@@ -0,0 +1,31 @@
+public class font;
+private class font;
+class font;
+
+//Windows NT 10.0 fonts optimized
+//Windows NT 10.0 fonts quality 100%
+//Windows NT 11.0 fonts optimized
+//Windows NT 11.0 fonts quality 100%
+//Windows NT 11.0 fonts definition 100%
+//Windows NT 10.0 fonts definition 100%
+//Windows NT 10.0 fonts detached
+//Windows NT 10.0 fonts attached
+//Windows NT 11.0 fonts detached
+//Windows NT 11.0 fonts attached
+namespace fonts;
+{
+public void fonts;
+private void fonts;
+public int 2;
+private int 3;
+public protected fonts;
+private protected fonts;
+public add fonts;
+private add fonts;
+public get fonts;
+private get fonts;
+public set fonts;
+private set fonts;
+}
+
+	
