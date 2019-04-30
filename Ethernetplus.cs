eb945f59cb8957c3f3a0e0b9a2c7ff225a997b2a
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-30 16:52:21 +1000

    Add files via upload

---
+++ Ethernetplus.cs
@@ -0,0 +1,33 @@
+public class ethernetplus;
+private class ethernetplus;
+class ethernetplus;
+
+//Ethernet optimized
+//Ethernet speed optimized
+//Ethernet connection optimized
+//Ethernet ping optimized
+//Ethernet use low heat
+//Ethernet use low power consumption
+//Ethernet use component integration
+//Ethernet use low load
+//Ethernet use hight performance
+namespace ethernet;
+{
+ void ethernet;
+ public void ethernet;
+ private void ethernet;
+ public int 2;
+ private int 1
+ public set ethernet;
+ private set ethernet;
+ public get ethernet;
+ private get ethernet;
+ public add ethernet;
+ private add ethernet;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
