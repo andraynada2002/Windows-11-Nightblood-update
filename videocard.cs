6e120aa32bc96f7b9b3aff20f9d90a2456200bf7
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-30 16:55:44 +1000

    Add files via upload

---
+++ videocard.cs
@@ -0,0 +1,44 @@
+public class Videocard;
+private class Videocard;
+class Videocard;
+
+//Videocard type processor
+//Videocard type graphics processor
+//Videocard optimized
+//Videocard hight performance
+//Videocard upgrading graphics
+//Videocard upgrade monitor RGB
+//Videocard HDR contrast monitor RGB
+//Videocard optimized performance
+//Videocard repaired performance
+//Videocard fixed graphics
+//Videocard fixed DirectX graphics
+//Videocard fixed Vulkan graphics
+//Videocard HDR bridthness monitor RGB
+//Videocard HDR histogram alignment
+//Videocard HDR histogram
+//Videocard detached
+//Videocard attached
+//Videocard detached performance
+//Videocard attached performance
+//Videocard use low power consumption
+//Videocard use low heat
+//Videocard use component integration
+//Videocard use low load
+//Videocard use hight performance
+namespace Videocard;
+{
+	public int 2;
+	public void Videocard()
+	public protected Videocard()
+	private int 3;
+	private void Videocard()
+	private protected Videocard()
+	public set Videocard;
+	public add Videocard;
+	public get Videocard;
+	private set Videocard;
+	private get Videocard;
+	private add Videocard;
+}
+
---
+++ videomemory.cs
@@ -0,0 +1,44 @@
+public class Videomemory;
+private class Videomemory;
+class Videomemory;
+
+//Videomemory type processor
+//Videomemory type graphics processor
+//Videomemory optimized
+//Videomemory hight performance
+//Videomemory upgrading graphics
+//Videomemory upgrade monitor RGB
+//Videomemory HDR contrast monitor RGB
+//Videomemory optimized performance
+//Videomemory repaired performance
+//Videomemory fixed graphics
+//Videomemory fixed DirectX graphics
+//Videomemory fixed Vulkan graphics
+//Videomemory HDR bridthness monitor RGB
+//Videomemory HDR histogram alignment
+//Videomemory HDR histogram
+//Videomemory detached
+//Videomemory attached
+//Videomemory detached performance
+//Videomemory attached performance
+//Videomemory use low power consumption
+//Videomemory use low heat
+//Videomemory use component integration
+//Videomemory use low load
+//Videomemory use hight performance
+namespace Videomemory;
+{
+	public int 2;
+	public void Videomemory()
+	public protected Videomemory()
+	private int 3;
+	private void Videomemory()
+	private protected Videomemory()
+	public set Videomemory;
+	public add Videomemory;
+	public get Videomemory;
+	private set Videomemory;
+	private get Videomemory;
+	private add Videomemory;
+}
+
