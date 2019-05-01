3c3aa701b44685cb1201c84ac886bab9e683a16e
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-01 14:28:21 +1000

    Add files via upload

---
+++ optimizer.cs
@@ -0,0 +1,45 @@
+public class optimizer;
+private class optimizer;
+class optimizer;
+
+//Windows NT 10.0 optimized
+//Windows NT 10.0 use registry optimized
+//Windows NT 10.0 use shell optimized
+//Windows NT 10.0 use core optimized
+//Windows NT 10.0 use services optimized
+//Windows NT 10.0 use toolbar optimized
+//Windows NT 10.0 use explorer optimized
+//Windows NT 10.0 use system applications optimized
+//Windows NT 10.0 use system dll optimized
+//Windows NT 10.0 use system optimized
+//Windows NT 10.0 use bios optimized
+//Windows NT 10.0 use desktop optimized
+//Windows NT 10.0 use DPI optimized
+//Windows NT 10.0 use screen resolution optimized
+//Windows NT 10.0 use DirectX 12.x optimized
+//Windows NT 10.0 use DirectX 11.x optimized
+//Windows NT 10.0 use DirectX 10.x optimized
+//Windows NT 10.0 use DirectX 9.x optimized
+//Windows NT 10.0 use Direct3D 12.x optimized
+//Windows NT 10.0 use Direct3D 11.x optimized
+//Windows NT 10.0 use Direct3D 10.x optimized
+//Windows NT 10.0 use Direct3D 9.x optimized
+//Windows NT 10.0 use Google Chrome optimized
+//Windows NT 10.0 use Opera optimized
+//Windows NT 10.0 use Microsoft Edge optimized
+namespace optimizer;
+{
+	public void optimizer;
+	private void optimizer;
+	public int 2;
+	private int 1;
+	public protected optimizer;
+	private protected optimizer;
+	public get optimizer;
+	private get optimizer;
+	public add optimizer;
+	private add optimizer;
+	public set optimizer;
+	private set optimizer;
+}
+
