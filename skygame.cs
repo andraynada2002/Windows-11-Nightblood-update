6b1503510ca351d13adc680a8a86cd8767c4f6f8
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-01 15:15:21 +1000

    Add files via upload

---
+++ skygame.cs
@@ -0,0 +1,24 @@
+public class skygame;
+private class skygame;
+class skygame;
+
+//Skygame type application optimized
+//Skygame type application
+//Skygame use optimized application
+//SKygame use optimized applications
+//Skygame use hight performance for applications
+//Skygame use optimized performance for applications
+{
+	public void skygame;
+	public int 2;
+	public protected skygame;
+	public get skygame;
+	public set skygame;
+	public add skygame;
+	private void skygame;
+	private int 3;
+	private protected skygame;
+	private get skygame;
+	private set skygame;
+	private add skygame;
+}
