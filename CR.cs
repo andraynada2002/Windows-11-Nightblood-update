668a61e109c670325a8e9b26ebb30fa0eace6fee
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-01 15:03:30 +1000

    Add files via upload

---
+++ CR.cs
@@ -0,0 +1,36 @@
+public class cr;
+private class cr;
+class cr;
+
+//Google Chrome auto detected version
+//Google Chrome auto detected yer version
+//Google Chrome auto detected directory
+//Google Chrome fixed performance
+//Google Chrome use low heat
+//Google Chrome use optimized performance
+//Google Chrome optimized
+//Google Chrome use optimized tabs
+//Google Chrome use optimized clicks
+//Google Chrome use optimized settings
+//Google Chrome use optimized sites
+//Google Chrome use optimized web
+//Google Chrome use optimized configuration
+//Google Chrome use optimized flash player
+//Google Chrome use optimized webgl
+//Google Chrome use optimized extensions
+//Google Chrome use optimized api
+namespace cr;
+{
+	public void cr;
+	public int 2;
+	public protected cr;
+	public get cr;
+	public set cr;
+	public add cr;
+	private void cr;
+	private int 1;
+	private protected cr;
+	private get cr;
+	private set cr;
+	private add cr;
+}
