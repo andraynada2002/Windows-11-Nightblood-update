f8b4b4dbbd074190848c6be5206db8f27da092c3
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-06 17:43:14 +1000

    Add files via upload

--- ram.cs
+++ ram.cs
@@ -1,60 +1,13 @@
-class ram;
+string Filename = @"ram.cs"
 public class ram;
-private class ram;
-
-//RAM optimized
-//RAM boosted
-//RAM type DDR3
-//RAM type DDR4
-//RAM optimized performance
-//RAM repaired performance
-//RAM hight performance
-//RAM optimized framerate
-//RAM optimized
-//RAM type physical memory
-//RAM detached
-//RAM attached
-//RAM detached performance
-//RAM attached performance
-//RAM use low power consumption
-//RAM use low heat
-//RAM use component integration
-//RAM use low load
-//RAM use hight performance
-//RAM use auto detect
-namespace ram;
+class ram;
 {
-	void ram()
-	public void ram()
-	private void ram()
-	int 2;
-	private int 3;
-	public int 4;
-	protected ram()
-	public protected ram()
-	private protected ram()
-	public set ram;
-	public get ram;
-	public add ram;
-	set ram;
-	get ram;
-	add ram;
-	private set ram;
-	private add ram;
-	private get ram;
+	//Windows type os
+	//Windows RAM optimized
+	//Windows RAM repaired performance
+	//Windows RAM repaired optimization
+	//Windows RAM repaired load
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
