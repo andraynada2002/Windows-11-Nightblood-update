d91f7adf5211a9f89fd06ccf0df253e5302396f8
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-06 17:40:12 +1000

    Add files via upload

---
+++ CPU.cs
@@ -0,0 +1,14 @@
+string Filename = @"CPU.cs"
+public class cpu;
+class cpu;
+{
+	//Windows type os
+	//Windows CPU optimized
+	//Windows CPU repaired performance
+	//Windows CPU repaired optimization
+	//Windows CPU repaired load
+}
+
+
+
+
