36fc63e0b3e84ab6c0cbba9fb6b3d1681d7c7220
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-06 17:41:44 +1000

    Add files via upload

---
+++ GPU.cs
@@ -0,0 +1,11 @@
+string Filename = @"GPU.cs"
+public class gpu;
+class gpu;
+{
+	//Windows type os
+	//Windows GPU optimized
+	//Windows GPU repaired performance
+	//Windows GPU repaired optimization
+	//Windows GPU repaired load
+}
+
