792c9080432de44b13b69beceed801aab019ce8c
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-14 15:58:45 +1000

    Add files via upload

---
+++ zpu.cs
@@ -0,0 +1,316 @@
+class AngelScript;
+static int Main(string[] args)
+{
+    simulate AngelScript;
+    return 0;
+}
+simulate ZPU;
+fixed.error CPU;
+fixed.error Central_processor;
+virtual simulate ZPU;
+simulator zero_processor;
+simulate zero_processor;
+simulate zero_processor(1);
+simualte zero_processor(2);
+simulate zero_processor(3);
+active ZPU(0);
+active ZPU(1);
+active zpu(2);
+active AngelScript;
+
+//ZPU virtual CPU
+//ZPU type processor
+//ZPU type Zero processor
+//ZPU type Graphics processor
+namespace ZPU;
+{
+    public void ZPU(0);
+    public public   ZPU { get; set; }
+    public region ZPU(0);
+}
+
+simulate zero_processor(0);
+simulate zero_processor(1);
+simulate zero_processor(2);
+simulate zero_processor(3);
+simulate ray_tracing(0);
+simulate ray_tracing(1);
+simulate ray_tracing(2);
+simulate ray_tracing(3);
+simulate GPU;
+simulate GPU(1);
+simulate GPU(0);
+simulate GPU(2);
+simulate performance;
+simulate performance(0);
+simulate performance(1);
+simulate performance(2);
+simulate performance(3);
+engine performance;
+engine zpu;
+engine cpu;
+engine gpu;
+engine public ZPU et
+    {
+        return new ZPU (this);
+    }
+}
+
+public class ZPU 
+{
+    readonly ClassName outer;
+    
+    internal ZPU (ClassName outer)
+    {
+        this.outer = outer;
+    }
+
+engine GPU2;
+engine ZPU(0);
+engine public ZPU0Iterator ZPU0
+{
+    get
+    {
+        return new ZPU0Iterator(this);
+    }
+}
+
+public class ZPU0Iterator
+{
+    readonly ClassName outer;
+    
+    internal ZPU0Iterator(ClassName outer)
+    {
+        this.outer = outer;
+    }
+    
+    // TODO: provide an appropriate implementation here
+    public int Length { get { return 1; } }
+    
+    public ElementType this[int index]
+    {
+        get
+        {
+            //
+            // TODO: implement indexer here
+            //
+            // you have full access to ClassName privates
+            //
+            throw new System.NotImplementedException();
+            return default(ElementType);
+        }
+    }
+    
+    public System.Collections.Generic.IEnumerator<ElementType> GetEnumerator()
+    {
+        for (int i = 0; i < this.Length; i++)
+        {
+            yield return this[i];
+        }
+    }
+}
+
+engine get_index(0); public System.Collections.Generic.IEnumerator<Get_index> GetEnumerator()
+{
+    throw new System.NotImplementedException();
+    yield return default(Get_index);
+}
+
+get enginegl;
+
+get gpu;
+
+get cpu;
+
+get zpu;
+
+find zpu;
+
+find zpu(0);
+
+find zpu(1);
+
+find zpu(2);
+
+find zpu(3);
+
+active AngelScript;
+get AngelScript;
+IEnumerator AngelScript;
+
+simulate hight_Performance;
+simulate virtual_physics;
+simulate virtual_graphics;
+
+deusing System.IO
+deusing System.IO1
+deusing core ClassName(Parameters)
+{
+    
+public public System.Collections.Generic.IEnumerator<ZPU> GetEnumerator()
+{
+    throw new System.NotImplementedException();
+    yield return default(ZPU);
+}
+    
+}
+
+public using System.Rax
+public using System.engine
+
+annex ZPU;
+annex ZPU(0);
+annex ZPU(1);
+annex ZPU(2);
+
+annex System.Cryengine
+annex System.Validation
+
+annex System.processor
+annex System.Operator
+annex System.Orbital
+
+simulate DirectX_9_0;
+simulate DirectX_10_0;
+simulate DirectX_11_0;
+simulate DirectX_12_0;
+simulate DirectX_12_1;
+
+create DirectX_13_0;
+annex DirectX_13_0;
+active DirectX_13_0;    
+annex DirectX.Validation
+annex DirectX.ray_tracing
+
+using Old.Techelogy
+using New.Techelogy
+using New.performance
+
+virtual graphics_card;
+virtual graphics_processor;
+virtual zero_processor;
+virtual Central_processor;
+
+using Graphics.Card 
+using System.ABC
+
+fixed_error Validation;
+fixed_error load;
+fixed_error performance;
+fixed_error lags;
+fixed_error freeze;
+fixed_error bugs;
+fixed_error glitches;
+
+annex debug;
+annex dump;
+annex ray_tracing;
+annex performance;
+annex HDR;
+
+simulate HDR;
+active HDR;
+active ray_tracing;
+
+fixed_error internet_connection;
+fixed_error internet_speed;
+fixed_error internet_ping;
+fixed_error rostelecom_provider;
+fixed_error paynet;
+
+simulate internet;
+simulate life;
+simulate lifecycle;
+
+simulate peformance_halfbrick;
+simulate performance_enginegl;
+simulate vulkan_api;
+
+post_engine directx;
+post_engine ZPU;
+
+virtual messager;
+virtual validation;
+virtual anticritical;
+
+simulate bios;
+simulate anticritical;
+simulate fixer;
+simulate fixed_zpu;
+simulate fixator;
+
+IEnumerator ZPU;
+public System.Collections.Generic.IEnumerator<Engine> GetEnumerator()
+{
+    throw new System.NotImplementedException();
+    yield return default(Engine);
+}
+
+simulate Wifi;
+simulate Ethernet;
+
+simulate ultra_hight_performance;
+simulate ZPU;
+simulate bios;
+simulate american_megatrends;
+
+fixed_error ZPU;
+fixed_error Validation;
+fixed_error american_megatrends;
+fixed_error performance;
+
+public System.Collections.Generic.IEnumerator<PastTracing> GetEnumerator()
+{
+    throw new System.NotImplementedException();
+    yield return default(PastTracing);
+}
+
+cental orbital_script;
+cental angel_script;
+cental AngelScript;
+
+{
+    oldGEN AngelScript;
+    NewGen AngelScript;
+}
+
+{
+    _newgen Geforce_videocard;
+    _newgen ZPU;
+    _newgen graphics;
+    _newgen physics;
+}
+
+default csharp;
+
+{
+    afterhope ZPU;
+    before ZPU;
+    after ZPU;
+    actor ZPU;
+    simulate physics;
+    simulate graphics;
+    simulate inversed_cinematics;
+    simulate animations;
+}
+
+simulate KKK;
+get 0x99999999;
+
+megadump ZPU;
+ultradump ZPU;
+using System.Script 
+using System.Xml
+deform Form;
+uniform Script;
+debug Console;
+debug ZPU;
+
+console.debug ZPU;
+console.Write ZPU;
+
+end ZPU;
+end GPU;
+end CPU;
+end ND;
+
