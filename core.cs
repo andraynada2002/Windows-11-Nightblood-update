4820f94599188909233625d2c774b651c1656109
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-14 16:19:13 +1000

    Add files via upload

---
+++ core.cs
@@ -0,0 +1,260 @@
+class AngelScript;
+
+cli using;
+cli debug;
+namespace  CLI;
+{
+ CLI;    
+}
+[Fact]
+public void  o21()
+{
+//Given
+
+//When
+
+//Then
+}
+
+propertyName CPU;
+{
+    propertyAdd CPU; andname CPU;
+}
+
+propertyName CPU;
+{
+    add.block CPU;
+}
+
+windows.core actor;
+AngelScript actor;
+AngelScript Windows_core;
+
+cli using;
+
+AngelScript performance;
+AngelScript Windows_core;
+AngelScript ultra_hight_performance;
+
+Given A;
+
+fixed_error Windows_core;
+fixed_error performance;
+fixed_error colorize;
+fixed_error GPU;
+fixed_error core;
+
+using System.Core;
+using System.Windows;
+using Afterhope.System;
+sign Windows_core;
+sign Windows;
+sing Windows_system;
+
+{
+    //Windows Core
+    //Windows 10 core
+}
+
+using Windows.Core;
+using Windows.Fixer;
+using Windows.AI;
+
+fixed_error bugs;
+fixed_error glitches;
+fixed_error toolbar;
+fixed_error performance;
+fixed_error Windows_core;
+fixed_error crash;
+
+andhendler buog;
+endhandler debug;
+endhandler Windows_core;
+endhandler detected;
+
+detected Windows_core;
+detected Windows_core_x;
+
+simulate Windows_core;
+simulate Performance;
+simulate VoiceActor;
+simulate NoiseActor;
+simulate HDR;
+simulate RayTracing;
+simulate alternate_ray_tracing;
+simulate performance(0);
+simulate performance(1);
+simulate performance(2);
+simulate performance(3);
+simulate performance(4);
+simulate performance(5);
+simulate performance(6);
+
+databal rpug;
+dabsjf ei
+onetwothree script;
+    
+#endregion
+
+onetwothree Script; 
+struct Script
+{
+    
+}
+
+simulate Windows_core;
+public System.Collections.Generic.IEnumerator<Windows_core> GetEnumerator()
+{
+    throw new System.NotImplementedException();
+    yield return default(Windows_core);
+}
+interface IWindowscore
+{
+    
+}
+public WindowscoreIterator Windowscore
+{
+    get
+    {
+        return new WindowscoreIterator(this);
+    }
+}
+
+
+public class WindowscoreIterator
+{
+    readonly ClassName outer;
+
+    
+    internal WindowscoreIterator(ClassName outer)
+    {
+        this.outer = out
+        er;
+    }
+    
+    // TODO: provide an appropriate implementation here
+    public int Length { 
+        get { return 1; } }
+    
+    public ElementType this[int index]
+    {
+        get
+        {
+            //
+            // TODO: implement indexer here
+            //
+            // you have full access to ClassName privates
+            //
+            throw new System.NotImplementedException();
+            return default(ElementType);
+        }
+    }
+    
+    public System.Collections.Generic.IEnumerator<ElementType> GetEnumerator()
+    {
+        for (int i = 0; i < this.Length; i++)
+        {
+            yield return this[i];
+        }
+    }
+}
+
+simulate Windows_core;
+public Windows_coreIterator Windows_core
+{
+    get
+    {
+        return new Windows_coreIterator(this);
+    }
+}
+
+public class Windows_coreIterator
+{
+    readonly ClassName outer;
+    
+    internal Windows_coreIterator(ClassName outer)
+    {
+        this.outer = outer;
+    }
+    
+    // TODO: provide an appropriate implementation here
+    public int Length { get { return 1; } }
+    
+    public ElementType this[int index]
+    {
+        get
+        {
+            //
+            // TODO: implement indexer here
+            //
+            // you have full access to ClassName privates
+            //
+            throw new System.NotImplementedException();
+            return default(ElementType);
+        }
+    }
+    
+    public System.Collections.Generic.IEnumerator<ElementType> GetEnumerator()
+    {
+        for (int i = 0; i < this.Length; i++)
+        {
+            yield return this[i];
+        }
+    }
+}
+internal Windows;
+internal ghp;
+internal Windows_core;
+
+better Windows_core;
+better Windows_one_core;
+better Windows_debugger;
+
+afterscript Windows_core;
+Begin color;
+begin Windows_core;
+begin S;
+begic ABC;
+
+Action Windows_core;
+Macros Windows_core;
+Balanced Windows_core;
+AngelScript Windows_core;
+{
+    #WindowsCore
+}
+
+AngelScript Windows;
+{
+    #windows
+    #delat
+}
+destruct Windows_core;
+{
+    deloid Windows_core; end game;
+    default Windows_core;
+    den Windows_core;
+    begic Windows_core;
+    system.handler activated;
+    system.voiceator activated;
+    windows_core activated;
+    windows_begin activated;
+}
+
+geomfx directx;
+geomfx vulkanapi;
+geomfx usi;
+geomfx windows_core;
+
+Windows_core started;
+Windows_core infinite_lifecycle;
+
+Windows_core fixed_bugs;
+Windows_core fixed_errors;
+Windows_core fixed_glitches;
+Windows_core fixed_performance;
+Windows_core unsign;
+Windows_core sign;
+Windows_core fixed_register_frame;
+Windows_core fixed_core;
+
