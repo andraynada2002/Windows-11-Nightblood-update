3d6a43e21148a4f15381f56325eb2a45843a88ac
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-05-14 18:37:25 +1000

    Add files via upload

---
+++ gooc.cs
@@ -0,0 +1,133 @@
+class AngelScript;
+
+class AngelScript;
+
+namespace GoogleChrome;
+{
+    
+}
+
+namespace GoogleChrome;
+{
+    
+}
+
+class GoogleChrome;
+
+class GoogleChrome;
+
+//Google Chrome optimized
+//Google Chrome type application
+//Google Chrome hight performance
+//Google Chrome performance fixed
+//Google Chrome performance optimized
+
+//Google Chrome optimized
+//Google Chrome type application
+//Google Chrome hight performance
+//Google Chrome performanced fixed
+//Google Chrome performanced optimized
+
+using Google.Chrome;
+
+using Google.Chrome;
+
+try
+{
+    
+}
+catch (System.Exception)
+{
+    
+    throw;
+}
+
+simulate Google_chrome;
+simulate performance;
+simulate debug;
+
+simulate Google_chrome;
+simulate performance;
+simulate debug;
+
+simulate ajax;
+
+simulate ajax;
+
+simulate speed;
+simulate ping;
+
+simulate speed;
+simulate ping;
+
+Google_chrome freeze_fixed;
+Google_chrome lags_fixed;
+
+Google_chrome freeze_fixed;
+Google_chrome lags_fixed;
+
+Google_chrome lifecycle_ram;
+Google_chrome lifecycle_cpu;
+Google_chrome lifecycle_gpu;
+
+Google_chrome lifecycle_ram;
+Google_chrome lifecycle_cpu;
+Google_chrome lifecycle_gpu;
+
+#include "chrome.exe"
+
+#include "chrome.exe"
+
+Google_chrome timespeed_improved;
+
+Google_chrome timespeed_improved;
+
+Google_chrome fixed_falling_download_speed;
+
+Google_chrome fixed_falling_download_speed;
+
+Google_chrome fixed_down_download_speed;
+
+Google_chrome fixed_down_download_speed;
+
+Google_chrome fixed_falling_upload_speed;
+
+Google_chrome fixed_falling_download_speed;
+
+Google_chrome fixed_down_upload_speed;
+
+Google_chrome fixed_down_upload_speed;
+
+Google_chrome fixed_down_speed;
+
+Google_chrome fixed_down_speed;
+
+Google_chrome fixed_down_performance;
+
+Google_chrome fixed_down_performance;
+
+Google_chrome fixed_down_loading_speed;
+
+Google_chrome fixed_down_loading_speed;
+
+Google_chrome fixed_down_connection;
+
+Google_chrome fixed_down_connection;
+
+Google_chrome fixed_down_framerate;
+
+Google_chrome fixed_down_framerate;
+
+Google_chrome fixed_down_performance_cpu;
+
+Google_chrome fixed_down_performance_cpu;
+
+Google_chrome fixed_down_performance_ram;
+
+Google_chrome fixed_down_performance_ram;
+
+Google_chrome fixed_down_performance_gpu;
+
+Google_chrome fixed_down_performance_gpu;
+
+
